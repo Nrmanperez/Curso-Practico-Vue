9a503f3f92fb7d7f61d16fe4c4bd73e89a0d1e35
Nrmanperez <nperez@nicarao.agency>
2022-06-30 16:58:38 -0600

    firts

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+        <template #resume>
+            <Resume
+                :label="'Ahorro total'"
+                :amount="10000"
+            />
+        </template>
+        <template #movements>
+            <Movements/>
+        </template>
+    </Layout>
+</template>
+
+<script>
+    import Layout from "./Layout.vue";
+    import Header from "./Header.vue";
+    import Resume from "./Resume/Index.vue";
+    import Movements from "./Movements.vue";
+
+    export default {
+        components: {
+    Layout,
+    Header,
+    Resume,
+    Movements
+}
+    }
+</script>
